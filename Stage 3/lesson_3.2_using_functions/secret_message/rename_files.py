fe9d30d0681ad707c9e80812f79ff1f8e5db7bea
Cynikal <bryanb514@comcast.net>
2016-08-15 19:36:27 -0700

    Chapter 3 file update

--- Stage 3/lesson_3.2_using_functions/secret_message/rename_files.py
+++ Stage 3/lesson_3.2_using_functions/secret_message/rename_files.py
@@ -13,3 +13,17 @@
 #
 
 # Your code here.
+
+import os
+
+
+def rename_files():
+    file_list = os.listdir(r"C:\Users\Cynikal\Google Drive\Udacity Nanodegree\Intro to Programming\udacity\Stage 3\lesson_3.2_using_functions\secret_message\prank")
+    saved_path = os.getcwd()
+    os.chdir(r"C:\Users\Cynikal\Google Drive\Udacity Nanodegree\Intro to Programming\udacity\Stage 3\lesson_3.2_using_functions\secret_message\prank")
+    for file_name in file_list:
+        new_name = file_name.translate(None, "0123456789")
+        os.rename(file_name, new_name)
+        print "Old name: " + file_name+"\nNew name: " + new_name
+    os.chdir(saved_path)
+print rename_files()
